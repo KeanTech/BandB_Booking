5541f000374704ed8397047974b7071d82045082
Kenneth Andersen <kenethanders@gmail.com>
2023-08-10 14:04:44 +0200

    Initial upload

---
+++ B&B_Api/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace B_B_Api.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
