a34e28009ab7fedf2229d9fa3a01f52febc08c05
Kenneth Andersen <kenethanders@gmail.com>
2023-08-10 15:21:13 +0200

    Added all controllers need and added the methods needed

---
+++ B&B_Api/Controllers/LoginController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace B_B_Api.Controllers
+{
+    [ApiController]
+    [Route("controller")]
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        [Route("Get")]
+        public IActionResult Get()
+        {
+            return NotFound();
+        }
+
+        [HttpPost]
+        [Route("Create")]
+        public IActionResult Post()
+        {
+            return NotFound();
+        }
+
+        [HttpDelete]
+        [Route("Delete")]
+        public IActionResult Delete()
+        {
+            return NotFound();
+        }
+
+        [HttpPost]
+        [Route("Update")]
+        public IActionResult Update()
+        {
+            return NotFound();
+        }
+    }
+}
