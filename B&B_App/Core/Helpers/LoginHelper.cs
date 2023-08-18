b2f988ccbff69b0bedccbf258d7702d7821d3fab
Kenneth Andersen <kenethanders@gmail.com>
2023-08-18 10:40:54 +0200

    Main page more or less done

---
+++ B&B_App/Core/Helpers/LoginHelper.cs
@@ -0,0 +1,15 @@
+﻿using B_B_ClassLibrary.BusinessModels;
+
+namespace B_B_App.Core.Helpers
+{
+    public class LoginHelper
+    {
+        public bool ValidateUser(User user) 
+        {
+
+            return true;
+        }
+
+
+    }
+}
--- B&B_ClassLibrary/BusinessModels/User.cs
+++ B&B_ClassLibrary/BusinessModels/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Net.NetworkInformation;
 using System.Text;
@@ -11,11 +12,17 @@ namespace B_B_ClassLibrary.BusinessModels
     {
         public int Id { get; set; }
         public DateTime Created { get; set; }
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string LastName { get; set; }
+        [Required]
         public string Email { get; set; }
+        [Required]
         public string Password { get; set; }
+        [Required]
         public string PhoneNumber { get; set; }
+        [Required]
         public string Country { get; set; }
     }
 }
