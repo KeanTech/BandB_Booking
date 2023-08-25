056a2880c02cd0189efa7bfa33a71a62ae784058
Kenneth Andersen <kenethanders@gmail.com>
2023-08-25 13:27:27 +0200

    Added required, and new page

--- B&B_ClassLibrary/BusinessModels/Location.cs
+++ B&B_ClassLibrary/BusinessModels/Location.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,12 +11,19 @@ namespace B_B_ClassLibrary.BusinessModels
     {
         public int Id { get; set; }
         public int LandlordId { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Address { get; set; }
+        [Required]
         public string City { get; set; }
+        [Required]
         public string ZipCode { get; set; }
+        [Required]
         public string Country { get; set; }
+        [Required]
         public int AmountOfRooms { get; set; }
+        [Required]
         public string? Area { get; set; }
         public double? Rating { get; set; }
         public Location()
